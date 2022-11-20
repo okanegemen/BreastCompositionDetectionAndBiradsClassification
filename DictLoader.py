3ed12b1feade1486a8fbe2d538c18be5cf3333f0
OkanEgemen <okanegemen.work@gmail.com>
2022-11-20 14:42:39 +0300

    requirements of frameworks

---
+++ DictLoader.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+from dataset import DICOM_Dataset
+
+
+
+if not torch.backends.mps.is_available():
+    if not torch.backends.mps.is_built():
+        print("MPS not available because the current PyTorch install was not "
+              "built with MPS enabled.")
+    else:
+        print("MPS not available because the current MacOS version is not 12.3+ "
+              "and/or you do not have an MPS-enabled device on this machine.")
+
+else:
+    mps_device = torch.device("mps")
+    print(mps_device)
